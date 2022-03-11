55a4082cab08dc49d74c262e6a0b46679aedb2d3
Naseer Ahmed <thenaseer.dev@gmail.com>
2022-03-11 15:39:34 +0500

    tailwind spinner

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Tailwind-Spinner
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
---
+++ pages/index.js
@@ -0,0 +1,360 @@
+
+
+export default function Home() {
+  return (
+    <>
+<div class="flex items-center justify-center min-h-screen bg-white">
+    <div class="flex flex-col">
+        <div class="flex space-x-24">
+            <div class="container space-y-10 relative">
+                <h1 class="mb-8 text-xl font-semibold">
+                    Regular Spinners
+                </h1>
+
+                <div class="flex flex-col">
+                    <h4 class="mb-8">
+                        Open Rings
+                    </h4>
+
+                    <div class="flex flex-row space-x-4">
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border border-solid border-yellow-500 border-t-transparent"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-2 border-solid border-blue-500 border-t-transparent"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-4 border-solid border-green-500 border-t-transparent"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-8 border-solid border-purple-500 border-t-transparent"></div>
+                    </div>
+                </div>
+
+                <div class="flex flex-col">
+                    <h4 class="mb-8">
+                        Closed Rings
+                    </h4>
+
+                    <div class="flex flex-row space-x-16">
+                        <div class="flex">
+                            <div class="relative">
+                                <div class="w-12 h-12 rounded-full absolute
+                            border border-solid border-gray-200"></div>
+
+                                <div class="w-12 h-12 rounded-full animate-spin absolute
+                            border border-solid border-yellow-500 border-t-transparent"></div>
+                            </div>
+                        </div>
+
+                        <div class="flex">
+                            <div class="relative">
+                                <div class="w-12 h-12 rounded-full absolute
+                            border-2 border-solid border-gray-200"></div>
+
+                                <div class="w-12 h-12 rounded-full animate-spin absolute
+                            border-2 border-solid border-blue-500 border-t-transparent"></div>
+                            </div>
+                        </div>
+
+                        <div class="flex">
+                            <div class="relative">
+                                <div class="w-12 h-12 rounded-full absolute
+                            border-4 border-solid border-gray-200"></div>
+
+                                <div class="w-12 h-12 rounded-full animate-spin absolute
+                            border-4 border-solid border-green-500 border-t-transparent"></div>
+                            </div>
+                        </div>
+
+                        <div class="flex">
+                            <div class="relative">
+                                <div class="w-12 h-12 rounded-full absolute
+                            border-8 border-solid border-gray-200"></div>
+
+                                <div class="w-12 h-12 rounded-full animate-spin absolute
+                            border-8 border-solid border-purple-500 border-t-transparent"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="flex flex-col">
+                    <h4 class="my-8">
+                        Edge X Rings
+                    </h4>
+
+                    <div class="flex flex-row space-x-4">
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-x border-solid border-yellow-500 border-t-transparent"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-x-2 border-solid border-blue-500 border-t-transparent"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-x-4 border-solid border-green-500 border-t-transparent"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-x-8 border-solid border-purple-500 border-t-transparent"></div>
+                    </div>
+                </div>
+
+                <div class="flex flex-col">
+                    <h4 class="mb-8">
+                        Edge Y Rings
+                    </h4>
+
+                    <div class="flex flex-row space-x-4">
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-y border-solid border-yellow-500 border-t-transparent"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-y-2 border-solid border-blue-500 border-t-transparent"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-y-4 border-solid border-green-500 border-t-transparent"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-y-8 border-solid border-purple-500 border-t-transparent"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container space-y-10 relative">
+                <h1 class="mb-8 text-xl font-semibold">
+                    Dashed Spinners
+                </h1>
+
+                <div class="flex flex-col">
+                    <h4 class="mb-8">
+                        Open Rings
+                    </h4>
+
+                    <div class="flex flex-row space-x-4">
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border border-dashed border-yellow-500 border-t-transparent"></div>
+
+                        
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-2 border-dashed border-blue-500 border-t-transparent"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-4 border-dashed border-green-500 border-t-transparent"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-8 border-dashed border-purple-500 border-t-transparent"></div>
+                    </div>
+                </div>
+
+                <div class="flex flex-col">
+                    <h4 class="mb-8">
+                        Closed Rings
+                    </h4>
+
+                    <div class="flex flex-row space-x-16">
+                        <div class="flex">
+                            <div class="relative">
+                                <div class="w-12 h-12 rounded-full absolute
+                            border border-dashed border-gray-200"></div>
+
+                                <div class="w-12 h-12 rounded-full animate-spin absolute
+                            border border-dashed border-yellow-500 border-t-transparent"></div>
+                            </div>
+                        </div>
+
+                        <div class="flex">
+                            <div class="relative">
+                                <div class="w-12 h-12 rounded-full absolute
+                            border-2 border-dashed border-gray-200"></div>
+
+                                <div class="w-12 h-12 rounded-full animate-spin absolute
+                            border-2 border-dashed border-blue-500 border-t-transparent"></div>
+                            </div>
+                        </div>
+
+                        <div class="flex">
+                            <div class="relative">
+                                <div class="w-12 h-12 rounded-full absolute
+                            border-4 border-dashed border-gray-200"></div>
+
+                                <div class="w-12 h-12 rounded-full animate-spin absolute
+                            border-4 border-dashed border-green-500 border-t-transparent"></div>
+                            </div>
+                        </div>
+
+                        <div class="flex">
+                            <div class="relative">
+                                <div class="w-12 h-12 rounded-full absolute
+                            border-8 border-dashed border-gray-200"></div>
+
+                                <div class="w-12 h-12 rounded-full animate-spin absolute
+                            border-8 border-dashed border-purple-500 border-t-transparent"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="flex flex-col">
+                    <h4 class="my-8">
+                        Edge X Rings
+                    </h4>
+
+                    <div class="flex flex-row space-x-4">
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-x border-dashed border-yellow-500 border-t-transparent"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-x-2 border-dashed border-blue-500 border-t-transparent"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-x-4 border-dashed border-green-500 border-t-transparent"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-x-8 border-dashed border-purple-500 border-t-transparent"></div>
+                    </div>
+                </div>
+
+                <div class="flex flex-col">
+                    <h4 class="mb-8">
+                        Edge Y Rings
+                    </h4>
+
+                    <div class="flex flex-row space-x-4">
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-y border-dashed border-yellow-500 border-t-transparent"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-y-2 border-dashed border-blue-500 border-t-transparent"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-y-4 border-dashed border-green-500 border-t-transparent"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-y-8 border-dashed border-purple-500 border-t-transparent"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container space-y-10 relative">
+                <h1 class="mb-8 text-xl font-semibold">
+                    Spinners With Shadow
+                </h1>
+
+                <div class="flex flex-col">
+                    <h4 class="mb-8">
+                        Open Rings
+                    </h4>
+
+                    <div class="flex flex-row space-x-4">
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border border-solid border-yellow-500 border-t-transparent shadow-md"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-2 border-solid border-blue-500 border-t-transparent shadow-md"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-4 border-solid border-green-500 border-t-transparent shadow-md"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-8 border-solid border-purple-500 border-t-transparent shadow-md"></div>
+                    </div>
+                </div>
+
+                <div class="flex flex-col">
+                    <h4 class="mb-8">
+                        Closed Rings
+                    </h4>
+
+                    <div class="flex flex-row space-x-16">
+                        <div class="flex">
+                            <div class="relative">
+                                <div class="w-12 h-12 rounded-full absolute
+                            border border-solid border-gray-200"></div>
+
+                                <div class="w-12 h-12 rounded-full animate-spin absolute
+                            border border-solid border-yellow-500 border-t-transparent shadow-md"></div>
+                            </div>
+                        </div>
+
+                        <div class="flex">
+                            <div class="relative">
+                                <div class="w-12 h-12 rounded-full absolute
+                            border-2 border-solid border-gray-200"></div>
+
+                                <div class="w-12 h-12 rounded-full animate-spin absolute
+                            border-2 border-solid border-blue-500 border-t-transparent shadow-md"></div>
+                            </div>
+                        </div>
+
+                        <div class="flex">
+                            <div class="relative">
+                                <div class="w-12 h-12 rounded-full absolute
+                            border-4 border-solid border-gray-200"></div>
+
+                                <div class="w-12 h-12 rounded-full animate-spin absolute
+                            border-4 border-solid border-green-500 border-t-transparent shadow-md"></div>
+                            </div>
+                        </div>
+
+                        <div class="flex">
+                            <div class="relative">
+                                <div class="w-12 h-12 rounded-full absolute
+                            border-8 border-solid border-gray-200"></div>
+
+                                <div class="w-12 h-12 rounded-full animate-spin absolute
+                            border-8 border-solid border-purple-500 border-t-transparent shadow-md"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="flex flex-col">
+                    <h4 class="my-8">
+                        Edge X Rings
+                    </h4>
+
+                    <div class="flex flex-row space-x-4">
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-x border-solid border-yellow-500 border-t-transparent shadow-md"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-x-2 border-solid border-blue-500 border-t-transparent shadow-md"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-x-4 border-solid border-green-500 border-t-transparent shadow-md"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-x-8 border-solid border-purple-500 border-t-transparent shadow-md"></div>
+                    </div>
+                </div>
+
+                <div class="flex flex-col">
+                    <h4 class="mb-8">
+                        Edge Y Rings
+                    </h4>
+
+                    <div class="flex flex-row space-x-4">
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-y border-solid border-yellow-500 border-t-transparent shadow-md"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-y-2 border-solid border-blue-500 border-t-transparent shadow-md"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-y-4 border-solid border-green-500 border-t-transparent shadow-md"></div>
+
+                        <div class="w-12 h-12 rounded-full animate-spin
+                    border-y-8 border-solid border-purple-500 border-t-transparent shadow-md"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div></>
+      
+  )
+}
